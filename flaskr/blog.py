d465843cd6ffc179882cb733514870935cb10de5
catta <atta.cherif@gmail.com>
2024-04-08 13:09:16 +0200

    feat(feat-107):build update ande delete views => functions && templates

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,5 @@
 
+from os import abort
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from flaskr.auth import login_required
 from flaskr.db import get_db
@@ -46,3 +47,60 @@ def create():
         flash(error, 'error')
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        updated_title = request.form['title']
+        updated_body = request.form['body']
+        error = None
+        if not updated_title:
+            error = 'Title is required.'
+        
+        if not error:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title=?, body=? WHERE id=?',
+                (updated_title, updated_body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+        flash(error, 'error')
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('GET', 'POST'))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute(
+        'DELETE FROM post WHERE id=?',
+        (id,)
+    )
+    db.commit()
+    
+    return redirect(url_for('blog.index'))
